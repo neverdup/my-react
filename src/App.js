6c6fc5c991218f807a3c23c498b54f0cb50588bd
Jaewon Yang <neverdup@gmail.com>
2022-08-08 17:49:20 +0900

    더하기 빼기

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 function App() {
   const [count, SetCount] = React.useState(1)
 
-  function addOne() {
-    SetCount(count + 1)
+  function changeCount(event) {
+    SetCount(count + Number(event.target.value) )
+    console.log(event)
   }
 
   return (
     <div className="App">
       <h1>{count}</h1>
-      <button className="add" onClick={addOne}>Add</button>
+      <button id="minus" value="-1" onClick={changeCount}>-</button>
+      <button id="plus" value="1" onClick={changeCount}>+</button>
     </div>
   );
 }
